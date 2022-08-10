7669e34072e39b30d865f061f5969ffb52950651
ramstime <ramstime@github.com>
2022-08-10 17:31:02 +0530

    added redis support

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 )
 
-var db = make(map[string]string)
+//var db = make(map[string]string)
+type Author struct {
+	Name  string   `json:"name"`
+	Age   int      `json:"age"`
+	Books []string `json:"books"`
+}
+type handler struct {
+	Client *redis.Client
+}
 
-func setupRouter() *gin.Engine {
+func (h handler) setupRouter() *gin.Engine {
 	// Disable Console Color
 	// gin.DisableConsoleColor()
 	r := gin.Default()
@@ -17,17 +29,8 @@ func setupRouter() *gin.Engine {
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
-
 	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
+	r.GET("user/:name", h.getDB)
 
 	// Authorized group (uses gin.BasicAuth() middleware)
 	// Same than:
@@ -41,34 +44,111 @@ func setupRouter() *gin.Engine {
 		"manu": "123", // user:manu password:123
 	}))
 
-	/* example curl for /admin with basicauth header
-	   Zm9vOmJhcg== is base64("foo:bar")
-
-		curl -X POST \
-	  	http://localhost:8080/admin \
-	  	-H 'authorization: Basic Zm9vOmJhcg==' \
-	  	-H 'content-type: application/json' \
-	  	-d '{"value":"bar"}'
-	*/
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
-	})
+	authorized.POST("admin", h.insertDB)
 
 	return r
 }
 
+/* example curl for /user/name with basicauth header
+curl -X GET   http://localhost:8080/user/foo
+curl -X GET   http://localhost:8080/user/fo*
+{"user":"foo","value":"{\"name\":\"rams\",\"age\":35,\"books\":null}"}
+*/
+func (h handler) getDB(c *gin.Context) {
+	author := c.Params.ByName("name")
+	fmt.Println("author: ", author)
+	if strings.Contains(author, "*") {
+		value, err := h.Client.Keys(c, author).Result()
+		if err != nil {
+			c.JSON(http.StatusNoContent, gin.H{"user": author, "status": "no value"})
+			fmt.Println(err)
+			return
+		}
+		var authorDetails []Author
+		for _, user := range value {
+			value, err := h.Client.Get(c, user).Result()
+			if err != nil {
+				c.JSON(http.StatusNoContent, gin.H{"user": user, "status": "no value"})
+				fmt.Println(err)
+				return
+			}
+			fmt.Printf("user: %v value: %v \n", user, value)
+			var authorData Author
+			if err := json.Unmarshal([]byte(value), &authorData); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"user": user, "status": "no value"})
+				fmt.Println(err)
+				return
+			}
+			authorDetails = append(authorDetails, authorData)
+		}
+		c.JSON(http.StatusOK, gin.H{"user": author, "value": authorDetails})
+		return
+	}
+	value, err := h.Client.Get(c, author).Result()
+	if err != nil {
+		c.JSON(http.StatusNoContent, gin.H{"user": author, "status": "no value"})
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("user: %v value: %v", author, value)
+	//value, ok := db[user]
+	c.JSON(http.StatusOK, gin.H{"user": author, "value": value})
+
+}
+
+/* example curl for /admin with basicauth header
+   Zm9vOmJhcg== is base64("foo:bar")
+
+	curl -X POST \
+  	http://localhost:8080/admin \
+  	-H 'authorization: Basic Zm9vOmJhcg==' \
+  	-H 'content-type: application/json' \
+  	-d '{"value":"bar"}'
+*/
+func (h handler) insertDB(c *gin.Context) {
+
+	user := c.MustGet(gin.AuthUserKey).(string)
+
+	author := Author{}
+	fmt.Println("user:", user)
+	if c.Bind(&author) == nil {
+
+		fmt.Println("author:", author)
+		jsonVal, err := json.Marshal(author)
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"user": user, "status": "no value"})
+			return
+		}
+		err = h.Client.Set(c, author.Name, jsonVal, 0).Err()
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"user": user, "status": "no value"})
+			return
+		}
+		val, err := h.Client.Get(c, author.Name).Result()
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"user": user, "status": "no value"})
+			return
+		}
+		fmt.Println(val)
+
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+		return
+	}
+	c.JSON(http.StatusInternalServerError, gin.H{"user": user, "status": "no value"})
+}
+
 func main() {
-	r := setupRouter()
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	handler := &handler{Client: client}
+	r := handler.setupRouter()
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
+
 }
--- main_test.go
+++ main_test.go
@@ -3,18 +3,48 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/go-redis/redismock/v8"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPingRoute(t *testing.T) {
-	router := setupRouter()
 
-	w := httptest.NewRecorder()
+	db, _ := redismock.NewClientMock()
+
+	handler := &handler{Client: db}
+	router := handler.setupRouter()
+
+	w1 := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/ping", nil)
-	router.ServeHTTP(w, req)
+	router.ServeHTTP(w1, req)
+	assert.Equal(t, 200, w1.Code)
+	assert.Equal(t, "pong", w1.Body.String())
 
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "pong", w.Body.String())
+	w2 := httptest.NewRecorder()
+	data := `{"name": "rams", "age":35, "books": ["solid design","galaxy", "avathar"] }`
+	req2, _ := http.NewRequest("POST", "/admin", strings.NewReader(data))
+	req2.Header.Add("Authorization", "Basic Zm9vOmJhcg==")
+	req2.Header.Add("Content-Type", "application/json")
+	router.ServeHTTP(w2, req2)
+	assert.Equal(t, 200, w2.Code)
+	assert.Regexp(t, `[a-z]+`, w2.Body.String())
+
+	w3 := httptest.NewRecorder()
+	req3, _ := http.NewRequest("GET", "/user/rams", nil)
+	router.ServeHTTP(w3, req3)
+	assert.Equal(t, 200, w3.Code)
+	assert.Regexp(t, `[a-z]+`, w3.Body.String())
+
+	//db, mock := redismock.NewClientMock()
+	// key := "rams"
+	// mock.ExpectGet(key).RedisNil()
+	// mock.Regexp().ExpectSet(key, `[a-z]+`, 30*time.Minute).SetErr(errors.New("FAIL"))
+	// mock.Regexp().ExpectGet(key).SetErr(errors.New("FAIL"))
+	// if err := mock.ExpectationsWereMet(); err != nil {
+	// 	t.Error(err)
+	// }
+
 }
